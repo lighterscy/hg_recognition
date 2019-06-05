81a66f392027f09f78f6896aba7b410ca680682a
lighterscy <lighterscy@outlook.com>
2019-06-05 11:13:14 +0800

    init label tool

--- utils/label_tool/annotator.py
+++ utils/label_tool/annotator.py
@@ -1,7 +1,34 @@
 import os
 from copy import deepcopy
 import cv2
-import utils.image_browser as ib
+import utils.label_tool.image_browser as ib
+
+
+WINDOW_NAME = 'selector'
+window_location = (100, 100)    # default window location
+
+_QUIT_KEY = 27
+_NEXT_KEY = ord('e')
+_LAST_KEY = ord('q')
+
+
+def _quit_func():
+    exit()
+
+
+def _next_func():
+    ib.next_image()
+
+
+def _previous_func():
+    ib.previous_image()
+
+
+func_map = {
+    _QUIT_KEY: _quit_func(),
+    _NEXT_KEY: _next_func(),
+    _LAST_KEY: _previous_func()
+}
 
 
 if __name__ == "__main__":
@@ -9,4 +36,15 @@ if __name__ == "__main__":
     ib.init(file_dir)
 
     json_path = os.path.join(file_dir, 'annotation.json')
+
+    cv2.namedWindow(WINDOW_NAME)
+    cv2.moveWindow(WINDOW_NAME, window_location[0], window_location[1])
+
+    while True:
+
+        key = cv2.waitKey(0)
+
+        if str(key) in func_map.keys():
+            func_map[key]()
+
 
