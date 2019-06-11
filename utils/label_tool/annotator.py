7ccf32ee00dfd9f2ab1071cc4a0b74f63fe7ade2
lighterscy <lighterscy@outlook.com>
2019-06-11 10:03:16 +0800

    basic label tool finished

--- utils/label_tool/annotator.py
+++ utils/label_tool/annotator.py
@@ -2,25 +2,31 @@ import os
 from copy import deepcopy
 import cv2
 import utils.label_tool.image_browser as ib
+import utils.label_tool.patch as pth
 
 
-WINDOW_NAME = 'selector'
+WINDOW_NAME = 'annotator'
 window_location = (100, 100)    # default window location
 
 _QUIT_KEY = 27
 _NEXT_KEY = ord('e')
 _LAST_KEY = ord('q')
 
+_patch_set = None
 
 _class_map = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '`': -1}
 
 
 def imshow():
     image = ib.current_image.copy()
+    if ib.frame_id in _patch_set.frame_dict.keys():
+        cv2.putText(image, 'type: '+str(_patch_set.frame_dict[ib.frame_id]), (0, 50), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 255), 5)
+    cv2.putText(image, ib.frame_id, (50, 200), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 255), 2)
     cv2.imshow(WINDOW_NAME, image)
 
 
 def _quit_func():
+    _patch_set.save_to_file(json_path)
     exit()
 
 
@@ -39,11 +45,17 @@ func_map = {
 }
 
 
+def new_frame(key):
+    patch = pth.Patch(ib.frame_id, int(key))
+    _patch_set.add(patch)
+
+
 if __name__ == "__main__":
-    file_dir = 'data'
+    file_dir = 'data/anno_test'
     ib.init(file_dir)
 
     json_path = os.path.join(file_dir, 'annotation.json')
+    _patch_set = pth.load_patch_set(json_path)
 
     cv2.namedWindow(WINDOW_NAME)
     cv2.moveWindow(WINDOW_NAME, window_location[0], window_location[1])
@@ -52,7 +64,8 @@ if __name__ == "__main__":
         imshow()
         key = cv2.waitKey(0)
         if chr(key) in _class_map.keys():
-            print(chr(key))
+            new_frame(chr(key))
+
         elif key in func_map.keys():
             func_map[key]()
 
--- utils/label_tool/patch.py
+++ utils/label_tool/patch.py
@@ -3,10 +3,32 @@ import cv2
 import copy
 
 
+class Patch(object):
+    def __init__(self, frame_id, type):
+        self.frame_id = frame_id
+        self.type = type
+        self.dict = {self.frame_id: self.type}
+
+
 class PatchSet(object):
     def __init__(self):
         self.frame_number = 0
-        self.frame_dict = {}
+        self.frame_dict = dict()
+
+    def save_to_file(self, full_path):
+        json_str = json.dumps(self.__dict__, indent=4)  # TODO dump changed
+        f = open(full_path, 'w')
+        f.write(json_str)
+        f.close()
+
+    def add(self, patch):
+        if patch.frame_id not in self.frame_dict.keys():
+            self.frame_number += 1
+        self.frame_dict.update(patch.dict)
+
+    def pop(self, frame_id):
+        self.frame_dict.pop(frame_id)
+        self.frame_number -= 1
 
 
 def load_patch_set(json_path):
@@ -17,12 +39,18 @@ def load_patch_set(json_path):
         print('cannot open ' + json_path + ', create a new one')
         with open(json_path, 'w+') as f:
             json_string = f.read()
+    patch_set = PatchSet()
 
     try:
         patch_dict = json.loads(json_string)
+        for frame_id, frame_type in patch_dict['frame_dict'].items():
+            patch = Patch(frame_id, frame_type)
+            patch_set.add(patch)
     except ValueError:
-        print(1111)
+        pass
+    return patch_set
 
 
-json_path = 'data/sample3.json'
-load_patch_set(json_path)
+if __name__ == '__main__':
+    test_path = 'data/anno_test/sample3.json'
+    load_patch_set(test_path)
